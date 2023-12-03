d4ca249e59224304c9419b91aab88ea837835271
jas <152701916+jastineedg@users.noreply.github.com>
2023-12-04 01:45:30 +0800

    Add files via upload

--- Kernel.cs
+++ Kernel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Threading;
 using Sys = Cosmos.System;
@@ -68,13 +68,14 @@ namespace PesOS
                 switch (splitted[0])
                 {
                     case "help":
-                        Console.WriteLine("These are the available commands of PesOS");
-                        Console.WriteLine("help - display the available commands");
-                        Console.WriteLine("info - display information of the Operating system");
-                        Console.WriteLine("clock - display the current date and time");
-                        Console.WriteLine("clear - clear the console");
-                        Console.WriteLine("restart - automatically restarts the operating system");
-                        Console.WriteLine("shutdown - automatically shutdowns the operating system");
+                        Console.WriteLine("These are the available commands in PesOS");
+                        Console.WriteLine("The 'help' command displays a list of available commands");
+                        Console.WriteLine("The 'info' command shows information about the Operating system");
+                        Console.WriteLine("The 'clock' command displays the current date and time");
+                        Console.WriteLine("The 'calculator' command enables the user to utilize a basic calculator");
+                        Console.WriteLine("The 'clear' command clears the console by removing the previous texts");
+                        Console.WriteLine("The 'restart' command automatically restarts the operating system");
+                        Console.WriteLine("The 'shutdown' command automatically shuts down the operating system");
                         //add more for added features
 
                         break;
@@ -90,6 +91,108 @@ namespace PesOS
                         Console.WriteLine(clock);
                         break;
 
+                    case "calculator":
+                        for (int i = 1; i == 1; i=i)
+                        {
+                            Console.Write("PesOS Calculator" + "\n\n" + "Input your operation" + "\n" +
+                                "Type '+' for Addition\n" +
+                                "Type '-' for Subtraction\n" +
+                                "Type '*' for Multiplication\n" +
+                                "Type '/' for Division\n" +
+                                "\nInput: ");
+
+                            var inputOperation = Console.ReadLine();
+
+                            if (inputOperation == "+")
+                            {
+                                Console.Write("\nInput two values to be added.\n\n");
+                                Console.Write("First Value: \n");
+                                int firstAddend = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Second Value: \n");
+                                int secondAddend = Convert.ToInt32(Console.ReadLine());
+                                int sum = firstAddend + secondAddend;
+                                Console.Write("\nThe Sum of First and Second Values is: " + sum + "\n");
+                            }
+
+                            else if (inputOperation == "-")
+                            {
+                                Console.Write("\nInput two values to be subtracted.\n\n");
+                                Console.Write("Minuend Value: \n");
+                                int minuend = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Subtrahend Value: \n");
+                                int subtrahend = Convert.ToInt32(Console.ReadLine());
+                                int difference = minuend - subtrahend;
+                                Console.Write("\nThe Difference of First and Second Values is: " + difference + "\n");
+                            }
+
+                            else if (inputOperation == "*")
+                            {
+                                Console.Write("\nInput two values to be multiplied.\n\n");
+                                Console.Write("Multiplicand Value: \n");
+                                int multiplicand = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Multiplier Value: \n");
+                                int multiplier = Convert.ToInt32(Console.ReadLine());
+                                int product = multiplicand * multiplier;
+                                Console.Write("\nThe Product of First and Second Values is: " + product + "\n");
+                            }
+
+                            else if (inputOperation == "/")
+                            {
+                                Console.Write("\nInput two values to be divided.\n\n");
+                                Console.Write("Dividend Value: \n");
+                                int dividend = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Divisor Value: \n");
+                                int divisor = Convert.ToInt32(Console.ReadLine());
+                                int quotient = dividend / divisor;
+                                Console.Write("\nThe Quotient of First and Second Values is: " + quotient + "\n");
+                            }
+
+                            else
+                            {
+                                Console.Write("\nPlease input a valid calculator operation.\n");
+                            }
+
+                            Console.Write("\nWould you like to use the calculator again? \n" + "Type 'Yes' or 'No'.\n\n" + "Input: ");
+                            var reCalculate = Console.ReadLine();
+                            if (reCalculate == "Yes")
+                            {
+                                i = 1;
+                            }
+
+                            else if (reCalculate == "No")
+                            {
+                                i = 0;
+                            }
+                          
+                            else
+                            {
+
+                                for (int j = 1; j == 1; j = j)
+
+                                {
+                                    Console.Write("\nPlease input either 'Yes' or 'No' only.\n" + "\nInput: ");
+                                    var repeatInput = Console.ReadLine();
+                                    if (repeatInput == "Yes")
+                                    {
+                                        i = 1; j = 0;
+                                    }
+
+                                    else if (repeatInput == "No")
+                                    {
+                                        i = 0; j = 0;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        j = 1;
+                                    }
+                                }
+                                
+                            }
+
+                        }
+                        break;
+
                     case "clear":
                         Console.Clear();
                         break;
@@ -103,7 +206,7 @@ namespace PesOS
                         break;
                         
                     default:
-                        Console.WriteLine("Command not found");
+                        Console.WriteLine("Command not found, type 'help' to see commands.\n");
                         break;
                         
                 }
