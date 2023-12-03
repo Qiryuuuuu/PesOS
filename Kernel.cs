720e538357f3d47f1798c62dcbf1b9d89abe6032
DaryllBanal1030 <152703170+DaryllBanal1030@users.noreply.github.com>
2023-12-03 12:32:37 +0800

    Add files via upload

--- Kernel.cs
+++ Kernel.cs
@@ -1,14 +1,17 @@
 ﻿using Cosmos.Core;
 using Cosmos.HAL;
 using System;
-using System.Data;
+using System.Collections.Generic;
 using System.Threading;
 using Sys = Cosmos.System;
 
 namespace PesOS
 {
     public class Kernel : Sys.Kernel
-    {// Display a centered title with a specified delay between characters
+    {
+        private List<User> users;
+        private User currentUser;
+
         private void ShowCenteredTitle(string title, int speed)
         {
             Console.Clear();
@@ -24,16 +27,34 @@ namespace PesOS
             }
 
             Console.WriteLine();
-            Thread.Sleep(2000); //This is the delay before the titles fade out
+            Thread.Sleep(2000);
             Console.Clear();
         }
 
         // This method is called before the main execution starts
         protected override void BeforeRun()
         {
+            users = new List<User>();
+            currentUser = null;
+
+            // Create a sample user
+            users.Add(new User("admin", "admin123"));
+
             ShowCenteredTitle("Innovation is the new Currency", 150);
             ShowCenteredTitle("PesOS", 400);
+
+            // Authenticate the user
+            while (currentUser == null)
+            {
+                Console.WriteLine("PesOS requires authentication.");
+                AuthenticateUser();
+            }
+
+            Console.WriteLine($"Welcome, {currentUser.Username}!");
+            Console.WriteLine($"Host Identity Code: {currentUser.HostIdentityCode}");
+
             Console.WriteLine("PesOS booted successfully. Type a line of text to get it echoed back.");
+            Console.WriteLine("Type 'restart' to restart the system or 'shutdown' to shut it down.");
         }
 
         // This method contains the main execution logic
@@ -44,147 +65,212 @@ namespace PesOS
             Console.Write("Text typed: ");
             Console.WriteLine(input);
 
-            Date checkDate = new Date();
-            Console.WriteLine("DATE and TIME");
-            checkDate.outputDate();
+            if (input.ToLower() == "restart")
+            {
+                RestartSystem();
+            }
+            else if (input.ToLower() == "shutdown")
+            {
+                ShutdownSystem();
+            }
+            else
+            {
+                Date checkDate = new Date();
+                Console.WriteLine("DATE and TIME");
+                checkDate.outputDate();
 
-            Date checkTime = new Date();
-            checkTime.outputTime();
+                Date checkTime = new Date();
+                checkTime.outputTime();
+            }
         }
 
+        private void AuthenticateUser()
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
 
-            
-            
+            currentUser = users.Find(u => u.Username == username && u.ValidatePassword(password));
+
+            if (currentUser == null)
+            {
+                Console.WriteLine("Authentication failed. User not found or invalid credentials. Try again.");
+            }
+        }
+
+        private void RestartSystem()
+        {
+            Console.WriteLine("Restarting the system...");
+            // Additional logic for restarting the system can be added here
+            Sys.Power.Reboot();
+        }
+
+        private void ShutdownSystem()
+        {
+            Console.WriteLine("Shutting down the system...");
+            // Additional logic for shutting down the system can be added here
+            Sys.Power.Shutdown();
+        }
     }
 
-    public class Date
+
+    public class User
     {
-        public static string CurrentDate(bool DisplayWeekday, bool DisplayYear)
+        public string Username { get; private set; }
+        private string PasswordHash { get; set; }
+        public string HostIdentityCode { get; private set; }
+
+        public User(string username, string password)
         {
-            string Weekday = "";
-            string Month = "";
-            if (DisplayWeekday)
-            {
-                switch (RTC.DayOfTheWeek)
-                {
-                    case 1:
-                        Weekday = "Monday, ";
-                        break;
-                    case 2:
-                        Weekday = "Tuesday, ";
-                        break;
-                    case 3:
-                        Weekday = "Wednesday, ";
-                        break;
-                    case 4:
-                        Weekday = "Thursday, ";
-                        break;
-                    case 5:
-                        Weekday = "Friday, ";
-                        break;
-                    case 6:
-                        Weekday = "Saturday, ";
-                        break;
-                    case 7:
-                        Weekday = "Sunday, ";
-                        break;
-                }
-            }
-            switch (RTC.Month)
+            Username = username;
+            PasswordHash = HashPassword(password);
+            HostIdentityCode = Guid.NewGuid().ToString();
+        }
+
+        private string HashPassword(string password)
+        {
+            // Use a secure hashing algorithm (e.g., bcrypt) in a real-world scenario
+            return password.GetHashCode().ToString();
+        }
+
+        public bool ValidatePassword(string inputPassword)
+        {
+            // Use a secure password verification mechanism in a real-world scenario
+            return PasswordHash == HashPassword(inputPassword);
+        }
+    }
+
+
+
+}
+
+public class Date
+{
+    public static string CurrentDate(bool DisplayWeekday, bool DisplayYear)
+    {
+        string Weekday = "";
+        string Month = "";
+        if (DisplayWeekday)
+        {
+            switch (RTC.DayOfTheWeek)
             {
                 case 1:
-                    Month = "January";
+                    Weekday = "Monday, ";
                     break;
                 case 2:
-                    Month = "February";
+                    Weekday = "Tuesday, ";
                     break;
                 case 3:
-                    Month = "March";
+                    Weekday = "Wednesday, ";
                     break;
                 case 4:
-                    Month = "April";
+                    Weekday = "Thursday, ";
                     break;
                 case 5:
-                    Month = "May";
+                    Weekday = "Friday, ";
                     break;
                 case 6:
-                    Month = "June";
+                    Weekday = "Saturday, ";
                     break;
                 case 7:
-                    Month = "July";
+                    Weekday = "Sunday, ";
                     break;
-                case 8:
-                    Month = "August";
-                    break;
-                case 9:
-                    Month = "September";
-                    break;
-                case 10:
-                    Month = "October";
-                    break;
-                case 11:
-                    Month = "November";
-                    break;
-                case 12:
-                    Month = "December";
-                    break;
-
             }
-            if (DisplayYear)
+        }
+        switch (RTC.Month)
+        {
+            case 1:
+                Month = "January";
+                break;
+            case 2:
+                Month = "February";
+                break;
+            case 3:
+                Month = "March";
+                break;
+            case 4:
+                Month = "April";
+                break;
+            case 5:
+                Month = "May";
+                break;
+            case 6:
+                Month = "June";
+                break;
+            case 7:
+                Month = "July";
+                break;
+            case 8:
+                Month = "August";
+                break;
+            case 9:
+                Month = "September";
+                break;
+            case 10:
+                Month = "October";
+                break;
+            case 11:
+                Month = "November";
+                break;
+            case 12:
+                Month = "December";
+                break;
+
+        }
+        if (DisplayYear)
+        {
+            if (DisplayWeekday)
             {
-                if (DisplayWeekday)
-                {
-                    return Weekday + RTC.DayOfTheMonth + "." + RTC.Month + ".20" + RTC.Year;
-                }
-                else
-                {
-                    return RTC.DayOfTheMonth + "." + RTC.Month + ".20" + RTC.Year;
-                }
+                return Weekday + RTC.DayOfTheMonth + "." + RTC.Month + ".20" + RTC.Year;
             }
             else
             {
-                if (DisplayWeekday)
-                {
-                    return Weekday + RTC.DayOfTheMonth + " " + Month;
-                }
-                else
-                {
-                    return RTC.DayOfTheMonth + " " + Month;
-                }
+                return RTC.DayOfTheMonth + "." + RTC.Month + ".20" + RTC.Year;
             }
-
         }
-        public void outputDate()
+        else
         {
+            if (DisplayWeekday)
+            {
+                return Weekday + RTC.DayOfTheMonth + " " + Month;
+            }
+            else
+            {
+                return RTC.DayOfTheMonth + " " + Month;
+            }
+        }
+
+    }
+    public void outputDate()
+    {
         Console.Write("Date:" + RTC.Month + "." + RTC.DayOfTheMonth + ".20" + RTC.Year + " ");
-        }
-        public static string CurrentTime(bool DisplaySeconds)
-        {
-            string hour = RTC.Hour.ToString();
-            if (hour.Length == 1) { hour = "0" + hour; }
-            string min = RTC.Minute.ToString();
-            if (min.Length == 1) { min = "0" + min; }
-            if (DisplaySeconds)
-            {
-                string sec = RTC.Second.ToString();
-                if (sec.Length == 1) { sec = "0" + sec; }
-                return hour + ":" + min + ":" + sec;
-            }
-            else
-            {
-                return hour + ":" + min;
-            }
-        }
-        public static string CurrentSecond()
+    }
+    public static string CurrentTime(bool DisplaySeconds)
+    {
+        string hour = RTC.Hour.ToString();
+        if (hour.Length == 1) { hour = "0" + hour; }
+        string min = RTC.Minute.ToString();
+        if (min.Length == 1) { min = "0" + min; }
+        if (DisplaySeconds)
         {
             string sec = RTC.Second.ToString();
             if (sec.Length == 1) { sec = "0" + sec; }
-            return sec;
+            return hour + ":" + min + ":" + sec;
         }
-        public void outputTime()
+        else
         {
-            Console.Write("Time:" + RTC.Hour.ToString() + ":" + RTC.Minute.ToString() + ":" + RTC.Second.ToString() + "\n");
+            return hour + ":" + min;
         }
     }
+    public static string CurrentSecond()
+    {
+        string sec = RTC.Second.ToString();
+        if (sec.Length == 1) { sec = "0" + sec; }
+        return sec;
+    }
+    public void outputTime()
+    {
+        Console.Write("Time:" + RTC.Hour.ToString() + ":" + RTC.Minute.ToString() + ":" + RTC.Second.ToString() + "\n");
+    }
 }
-
